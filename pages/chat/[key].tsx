6c666363d095fd27a88445f27e66aa3a7fe445ad
Ishaq Ibrahim <ishaq.ibrahim@convertdigital.com.au>
2023-05-09 13:24:45 +0500

    add footer to chat page

--- pages/chat/[key].tsx
+++ pages/chat/[key].tsx
@@ -101,9 +101,41 @@ export default function Page({
           content="View your ChatGPT chat and share it with everyone else!"
         />
         <link rel="icon" href="/128x128.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
       </Head>
       <Navbar />
-      <div className="bg-gray-200 dark:bg-gray-800">{transformedHtml}</div>;
+      <div className="bg-gray-200 dark:bg-gray-800">{transformedHtml}</div>
+      <footer className="py-8 px-4 text-center bg-gradient-to-r from-[#8B0000] to-[#FFFFAF]">
+        <p className="font-poppins mb-4">Made with ❤️ by Ishaq Ibrahim</p>
+        <div className="flex justify-center space-x-4">
+          <a
+            href="https://github.com/ishaqibrahimbot"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white hover:text-[#F0F0F0] transition-colors duration-300"
+          >
+            <i className="fab fa-github text-2xl"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ishaq-ibrahim/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white hover:text-[#F0F0F0] transition-colors duration-300"
+          >
+            <i className="fab fa-linkedin text-2xl"></i>
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
